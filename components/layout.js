cd9c76b035cc7f4f19694b50fbb425566019425b
Telikz <129996395+Telikz@users.noreply.github.com>
2023-11-13 14:49:00 +0100

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -37,7 +37,7 @@ export default function Layout({ children, home }) {
 
       <footer class="footer bg-cover text-base-content mt-auto font-extralight">
         <div>
-          <p className='text-xs'>©2023 Minmax.</p>
+          <p xmlns:cc="http://creativecommons.org/ns#" xmlns:dct="http://purl.org/dc/terms/"><a property="dct:title" rel="cc:attributionURL" href="http://minmaxing.net">Minmax</a> by <span property="cc:attributionName">Robin Olsen</span> is licensed under <a href="http://creativecommons.org/licenses/by-sa/4.0/?ref=chooser-v1" target="_blank" rel="license noopener noreferrer" style="display:inline-block;">CC BY-SA 4.0<img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/cc.svg?ref=chooser-v1"><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/by.svg?ref=chooser-v1"><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/sa.svg?ref=chooser-v1"></a></p>
         </div>
       </footer>
     </div>
